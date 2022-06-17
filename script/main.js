e1a8d0a79896caf0204e6617512b2fca471f85e7
jerry george <jerrygithub360@gmail.com>
2022-06-17 15:42:03 -0700

    blabla

--- script/main.js
+++ script/main.js
@@ -1,33 +1,21 @@
-class Editor {
-    constructor(){
-        this.states = {
-            bold:false,
-            italic:false,
-            underline:false
-        }
-        this.editorFrame = document.querySelector('[data-editor-frame]')
-        this.editorBlock = this.editorFrame.querySelector('[data-editor-block]')
-        this.boldBtn = this.editorFrame.querySelector('[data-editor-btn]')
-        this.underLineBtn = this.editorFrame.querySelector('[data-editor-btn]')
-        this.editorElement = {
-            bold:'b',
-            italic:'i',
-            underline:'u'
-        }
-    }
+let format= new function(){
+    
+        this.bold;
+        this.italic;
+        this.underline;
+        this.strikethrough;
+        this.superscript;
+        this.code;
+        this.paragraph;
+        this.blockquote;
+        this.div;
+        this.pre;
+        this.align;
 
-    boldAction(){
-        
-    }
-    setCaret() {
-        
-        var range = document.createRange()
-        var sel = window.getSelection()
+    
+   
 
-        range.setStart(this.editorBlock.childNodes[this.editorBlock.childNodes.length -1 ], 5)
-        range.collapse(true)
+    this.formats=[]
 
-        sel.removeAllRanges()
-        sel.addRange(range)
-    }
+
 }

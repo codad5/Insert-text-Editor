bd6e06dc36c7a675124f6f85e773547fc75b054f
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2022-06-18 22:17:48 +0100

    'nothing'

--- script/main.js
+++ script/main.js
@@ -2,13 +2,16 @@
 const $ = (selector) => document.querySelector(selector)
 let i = 0, boldStatus = false
 
-const  setCaret = () => {
-    var el = $('[data-editor-block]')
+const  setCaret = (selector = null) => {
+    var el = selector == null ? $('[data-editor-block]') : $('[data-editor-block]')
     var range = document.createRange()
     var sel = window.getSelection()
     console.log(el.childNodes[el.childNodes.length - 1])
-    range.setStart(el.childNodes[el.childNodes.length - 1], el.childNodes[el.childNodes.length - 1].length + 2)
-    range.collapse(true)
+    range.setStart(el.childNodes[el.childNodes.length - 1].childNodes[0] || el.childNodes[el.childNodes.length - 1], 0)
+    range.setEnd(el.childNodes[el.childNodes.length - 1].childNodes[0] || el.childNodes[el.childNodes.length - 1], 0)
+    console.log(range)
+    range.collapse()
+    
 
     sel.removeAllRanges()
     sel.addRange(range)
@@ -20,8 +23,8 @@ $('[data-bold-btn]').addEventListener('click', (e) => {
     $('[data-editor-block]').innerHTML = $('[data-editor-block]').innerHTML.trim()
     if(boldStatus) {
 
-        $('[data-editor-block]').innerHTML += `<b>&nbsp </b>`
-        $('[data-editor-block]').innerHTML += `<b>&nbsp </b>`
+        $('[data-editor-block]').innerHTML += `<b> </b>`
+        // $('[data-editor-block]').innerHTML += `<b>&nbsp </b>`
     } 
     setCaret();
 })
